ef4086ca5c48ee4512ebe4c7612a8752c4fab3a7
AsutoshMohapatra123 <asutosh.mohapatra@wegalvanize.com>
2020-01-29 14:00:37 +0530

    Switch case program

---
+++ 2.Go_Training/SwitchcaseGolang.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x = "suresh"
+	var y = "Asutosh"
+	var array = []string{"Suresh", "Asutosh", "Biswa"}
+
+	for i := 0; i < len(array)-1; i++ {
+		fmt.Print("Go runs on ")
+		switch x := Suresh; x {
+
+		case "Asutosh":
+			fmt.Println("asutosh - Automation Engineer")
+		default:
+			// freebsd, openbsd,
+			// plan9, windows...
+			fmt.Printf("%s.\n", array[i])
+		}
+	}
+}
